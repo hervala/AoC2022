add83bd282f766a049c4eeba793e2715e1093fcf
Jukka Hervala <jukka.hervala@haahtela.fi>
2022-12-03 08:46:07 +0200

    Day03

---
+++ AoC2022/Day04.cs
@@ -0,0 +1,16 @@
+public class Day04 : PuzzleDay
+{
+    public override void Part01()
+    {
+        throw new NotImplementedException();
+    }
+
+    public override void Part02()
+    {
+        throw new NotImplementedException();
+    }
+
+    protected override string InputExample => @"";
+
+    protected override string Input => @"";
+}
